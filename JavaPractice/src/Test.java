74211dd1edc1c275d1060997ef109c36ecd725ec
zeesh <zeesh@192.168.0.9>
2022-12-26 17:09:11 +0100

    Mein erstes commit

---
+++ JavaPractice/src/Test.java
@@ -0,0 +1,75 @@
+public class Test {
+
+	public static void main(String[] args) {
+		
+		Employee emp = new Employee(111, "Smith");
+		
+		Employee emp3 = new Employee();
+		emp3.setId(78).setName("Mark");
+		System.out.println("Chaining "+emp3.getId()+" "+emp3.getName());
+		
+		EmployeeService empService = new EmployeeService();
+		
+		empService.save(emp);
+		
+		Employee emp2 = empService.getEmployee();
+		
+		//emp2.getId().getName();
+		
+		
+		int id      =  emp2.getId();
+		String name = emp2.getName().toUpperCase();
+		
+		System.out.println("get the Data: "+id+" name: "+name);
+
+		
+	}
+}
+class Employee{
+	
+	int id;
+	String name;
+	
+	public Employee(int id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+	public Employee()
+	{
+		
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public Employee setId(int id) {
+		this.id = id;
+		return this;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public Employee setName(String name) {
+		this.name = name;
+		return this;
+	}
+	
+	
+	
+}
+class EmployeeService{
+	
+	public void save(Employee emp) 
+	{
+		System.out.println("save the id and name id: "+emp.getId()+" name: "+emp.getName());
+	}
+	
+	public Employee getEmployee()
+	{
+		Employee employee = new Employee(112, "John T");
+		return employee;
+	}
+}
