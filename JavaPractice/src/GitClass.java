c2dceddb0280ef93283e8b8429de6eda86c4a757
zeesh <zeesh@192.168.0.9>
2022-12-27 11:15:42 +0100

    3rd commit

--- JavaPractice/src/GitClass.java
+++ JavaPractice/src/GitClass.java
@@ -1,7 +1,10 @@
+import java.util.jar.Attributes.Name;
 
 public class GitClass {
 
 	public String name;
+	
+	//name = "";
 	//main Method
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
