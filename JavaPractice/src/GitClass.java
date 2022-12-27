5cd3d2b567ccb37aa412e7922e9dd21d4044ca69
zeesh <zeesh@192.168.0.9>
2022-12-27 11:37:12 +0100

    another method return null

--- JavaPractice/src/GitClass.java
+++ JavaPractice/src/GitClass.java
@@ -4,6 +4,7 @@ public class GitClass {
 
 	
 	public String name;
+	//name="test";
 	
 	void test() {
 		
@@ -12,7 +13,13 @@ public class GitClass {
 	//main Method
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		String name;
+		name="Hello";
 
+	}
+	static String text()
+	{
+		return null;
 	}
 
 }
